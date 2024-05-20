701b9c7c66dd8312b2a1bcfd52029ebe37b79896
zuoyou <cdy1998cdy@163.com>
2024-05-20 23:11:47 +0800

    :bug: fix: 导出

---
+++ packages/tools/index.ts
@@ -0,0 +1,30 @@
+// A group of functions that check the types of core JavaScript values.
+export { default as isObject } from "./lib/isObject";
+export { default as isNull } from "./lib/isNull";
+export { default as isUndef } from "./lib/isUnDef";
+export { default as isBoolean } from "./lib/isBoolean";
+export { default as isElement } from "./lib/isElement";
+export { default as isString } from "./lib/isString";
+export { default as isNumber } from "./lib/isNumber";
+export { default as isDate } from "./lib/isDate";
+export { default as isArray } from "./lib/isArray";
+export { default as isFunction } from "./lib/isFunction";
+export { default as isArguments } from "./lib/isArguments";
+
+// export { default as isRegExp } from "./lib/isRegExp";
+//
+// export { default as isSymbol } from "./lib/isSymbol";
+// export { default as isArrayBuffer } from "./lib/isArrayBuffer";
+// export { default as isError } from "./lib/isError";
+//
+// export { default as isNaN } from "./lib/isNaN";
+// export { default as isMap } from "./lib/isMap";
+// export { default as isWeakMap } from "./lib/isWeakMap";
+// export { default as isSet } from "./lib/isSet";
+// export { default as isWeakSet } from "./lib/isWeakSet";
+//
+//
+// export { default as isTypedArray } from "./lib/isTypedArray";
+// export { default as isEmpty } from "./lib/isEmpty";
+// export { default as isMatch } from "./lib/isMatch";
+// export { default as isEqual } from "./lib/isEqual";
